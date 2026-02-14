0a02a3928fa12ec97d2b7cd876a0ab7f7b90017d
Visanu Sanyalak <visanu.sanyalak@gmail.com>
2026-02-14 10:51:12 +0700

    Add files via upload

---
+++ SAUCEDEMO-Playwright/TypeScript/config/testdata.ts
@@ -0,0 +1,11 @@
+// Normally you don't push secret to github !
+const user = {
+    "standard": "standard_user",
+    "locked": "locked_out_user",
+    "probmel": "problem_user",
+    "performance": "performance_glitch_user",
+    "error": "error_user",
+    "visual": "visual_user",
+}
+
+const password = "secret_sauce"
